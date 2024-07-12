6453a9b2c42c2ee42e633afe8652aa534fcc1717
Bathophobie <38230414+0x7da@users.noreply.github.com>
2024-07-12 17:43:02 +0200

    Update database.js

---
+++ utils/database.js
@@ -0,0 +1,9 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(process.env.DATABASE_PATH || 'ratings.db');
+
+db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS ratings (userId TEXT PRIMARY KEY, rating INTEGER)');
+    db.run('CREATE TABLE IF NOT EXISTS channels (guildId TEXT PRIMARY KEY, rateChannelId TEXT, logChannelId TEXT)');
+});
+
+module.exports = db;
